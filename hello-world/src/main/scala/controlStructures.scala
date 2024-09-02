34b24639f9bb7618babd26ed3c13b856a36a8c2d
André Pereira <andrepereira97@yahoo.com>
2024-09-01 21:27:59 -0300

    :sparkles: add control structures demo

---
+++ hello-world/src/main/scala/controlStructures.scala
@@ -0,0 +1,21 @@
+def negativePositive(x: Int): Int = {
+  if x < 0 then
+    println(s"$x is negative")
+  else if x == 0 then
+    println(s"$x is zero")
+  else
+    println(s"$x is positive")
+  x
+}
+
+def comparableTernary(a: Int, b: Int): Int = {
+  if a > b then a else b
+}
+
+@main def controlStructures(): Unit = {
+  negativePositive(0)
+  negativePositive(1)
+  negativePositive(-1)
+  comparableTernary(1, 2)
+  comparableTernary(2, 1)
+}
