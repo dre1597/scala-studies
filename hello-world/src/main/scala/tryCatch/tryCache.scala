a9065e3a7a13a151097211c775a6f87b22473ab8
André Pereira <andrepereira97@yahoo.com>
2025-02-09 16:22:11 -0300

    :sparkles: add try catch demo

---
+++ hello-world/src/main/scala/tryCatch/tryCache.scala
@@ -0,0 +1,9 @@
+package tryCatch
+
+try
+  writeTextToFile(text)
+catch
+  case ioe: IOException => println("Got an IOException.")
+  case nfe: NumberFormatException => println("Got a NumberFormatException.")
+finally
+  println("Clean up your resources here.")
